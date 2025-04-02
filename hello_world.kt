e3a4116fe84b8cc661cd0b28608927a0c3a9d8ce
Alberto Ledesma <alberto.ledesma.dev@gmail.com>
2025-04-01 20:10:05 -0600

    complete hello world

---
+++ hello_world.kt
@@ -0,0 +1,13 @@
+fun main() {
+    println("Hello")
+    
+    // val are read-only variables
+    val popcorn = 5
+    // var are mutable variables
+    var hotdog = 7
+    println(hotdog)
+    hotdog = 8
+    
+    // String templates and template expressions
+    println("popcornes $popcorn but pocornes + hotdogs = ${popcorn + hotdog}")
+}
